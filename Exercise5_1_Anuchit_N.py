46788228dd802d18be2ce080d1dd92d6bc33ce5e
ChampAnuchit <61983488+ChampAnuchit@users.noreply.github.com>
2020-07-29 19:04:40 +0700

    Create Exercise5_1_Anuchit_N.py
    5 - 1

---
+++ Exercise5_1_Anuchit_N.py
@@ -0,0 +1,14 @@
+a = int(input("Number_1 :"))
+b = int(input("Number_2 :"))
+
+result_Addition = a + b
+print(f'{a} + {b} = {result_Addition}')
+
+result_Deletion = a - b
+print(f'{a} - {b} = {result_Deletion}')
+
+result_Multiplication = a * b
+print(f'{a} * {b} = {result_Multiplication}')
+
+result_Division = a / b
+print(f'{a} / {b} = {result_Division}')
