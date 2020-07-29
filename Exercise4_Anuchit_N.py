4792118f80cf8c2c4f3eff574b4fa712d903f7ea
ChampAnuchit <61983488+ChampAnuchit@users.noreply.github.com>
2020-07-29 14:17:46 +0700

    Create Exercise4_Anuchit_N.py
    Homwork

---
+++ Exercise4_Anuchit_N.py
@@ -0,0 +1,9 @@
+English_point = 50;
+Business_point = 80;
+Computer_Systems = 70;
+Computer_Programming = 80;
+
+print("Foundation English : ", English_point)
+print("General Business : ", Business_point)
+print("Introduction to Computer Systems : ", Computer_Systems)
+print("Computer Programming : ", Computer_Programming)
