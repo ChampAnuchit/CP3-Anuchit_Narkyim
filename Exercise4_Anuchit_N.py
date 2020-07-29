e942c9346374a875ba6e30d13b2b778b80deedd7
ChampAnuchit <61983488+ChampAnuchit@users.noreply.github.com>
2020-07-29 14:22:04 +0700

    Update Exercise4_Anuchit_N.py
    Edit to decimal

--- Exercise4_Anuchit_N.py
+++ Exercise4_Anuchit_N.py
@@ -1,7 +1,7 @@
 English_point = 50;
-Business_point = 80;
-Computer_Systems = 70;
-Computer_Programming = 80;
+Business_point = 80.4;
+Computer_Systems = 70.9;
+Computer_Programming = 80.9;
 
 print("Foundation English : ", English_point)
 print("General Business : ", Business_point)
