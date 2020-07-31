6ce79a4bdd00cdce7c1bb1bf51772474c46c2de8
ChampAnuchit <61983488+ChampAnuchit@users.noreply.github.com>
2020-07-31 13:07:25 +0700

    Create Lecture_50_Anuchit_N.py
    Lecture_50 Homework

---
+++ Lecture_50_Anuchit_N.py
@@ -0,0 +1,48 @@
+# def AddNumber(x,y):
+#     print("การบวก =",x + y)
+# numA = int(input("Num1 = "))
+# numB = int(input("Num2 = "))
+# AddNumber(numA,numB)
+#
+# def DeleteNumber(x,y):
+#     print("การลบ =",x - y)
+# numC = int(input("Num1 = "))
+# numD = int(input("Num2 = "))
+# DeleteNumber(numC,numD)
+#
+# def MultiplicationNumber(x,y):
+#     print("การคูณ =",x * y)
+# numE = int(input("Num1 = "))
+# numF = int(input("Num2 = "))
+# MultiplicationNumber(numE,numF)
+#
+# def DivisionNumber(x,y):
+#     print("การหาร =",x / y)
+# numG = int(input("Num1 = "))
+# numH = int(input("Num2 = "))
+# DivisionNumber(numG,numH)
+
+
+def AddNumber(x,y):
+    print("การบวก =",x + y)
+
+
+def DeleteNumber(x,y):
+    print("การลบ =",x - y)
+
+
+def MultiplicationNumber(x,y):
+    print("การคูณ =",x * y)
+
+
+def DivisionNumber(x,y):
+    print("การหาร =",x / y)
+
+
+numA = int(input("Num1 = "))
+numB = int(input("Num2 = "))
+
+AddNumber(numA,numB)
+DeleteNumber(numA,numB)
+MultiplicationNumber(numA,numB)
+DivisionNumber(numA,numB)
